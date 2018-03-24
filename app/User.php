77b27c7f69cd8efff57c42f2b257fd5a8a761fa9
HuzarO <huzaro37512@gmail.com>
2018-03-24 19:16:28 +0100

    Latest Updates

--- app/User.php
+++ app/User.php
@@ -4,26 +4,30 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+class User extends Authenticatable {
+	use HasApiTokens, Notifiable;
+	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'username', 'email', 'password',
+	];
+	
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+	
+	public function findForPassport($identifier) {
+		return $this->orWhere('email', $identifier)->orWhere('username', $identifier)->first();
+	}
 }
